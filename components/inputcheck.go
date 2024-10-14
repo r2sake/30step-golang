e4524b6bf37524a801d6cff7a3a160b81068de7d
r2Sake <work_yoimaro@icloud.com>
2024-10-14 20:55:01 +0900

    inputcheck処理の最適化

--- components/inputcheck.go
+++ components/inputcheck.go
@@ -7,39 +7,29 @@ import (
 )
 
 func InputCheckForSampleCode(inStr []string, step int) (int, bool) {
-	if len(inStr) < 1 {
-		fmt.Println(fmt.Sprintf("Usage: go run step%02d/sample/main.go sample<sample_no>", step))
-		return 0, false
-	}
-	sampleName := os.Args[1]
-	if len(sampleName) < 7 || sampleName[:6] != "sample" {
-		fmt.Printf("Invalid sample name: %s\n", sampleName)
-		fmt.Println("Example name should be in the format 'sample<number>'")
-		return 0, false
-	}
-	sampleNo, err := strconv.Atoi(sampleName[6:])
-	if err != nil {
-		fmt.Printf("Invalid sample number: %s\n", sampleName[6:])
-		return 0, false
-	}
-	return sampleNo, true
+	return inputCheck("sample", inStr, step)
 }
 
 func InputCheckForExercisesCode(inStr []string, step int) (int, bool) {
-	if len(inStr) < 1 {
-		fmt.Println(fmt.Sprintf("Usage: go run step%02d/exercises/main.go exercises<exercises_no>", step))
+	return inputCheck("exercises", inStr, step)
+}
+
+func inputCheck(t string, i []string, s int) (int, bool) {
+	typeLen := len(t)
+	if len(i) < 1 {
+		fmt.Println(fmt.Sprintf("Usage: go run step%02d/%s/main.go %s<%s_no>", s, t, t, t))
 		return 0, false
 	}
-	exercisesName := os.Args[1]
-	if len(exercisesName) < 10 || exercisesName[:9] != "exercises" {
-		fmt.Printf("Invalid exercises name: %s\n", exercisesName)
-		fmt.Println("Example name should be in the format 'exercises<number>'")
+	ns := os.Args[1]
+	if len(ns) < typeLen+1 || ns[:typeLen] != t {
+		fmt.Println(fmt.Sprintf("Invalid %s name: %s\n", t, ns))
+		fmt.Println(fmt.Sprintf("Example name should be in the format '%s<number>'", t))
 		return 0, false
 	}
-	exercisesNo, err := strconv.Atoi(exercisesName[9:])
+	no, err := strconv.Atoi(ns[typeLen:])
 	if err != nil {
-		fmt.Printf("Invalid exercises number: %s\n", exercisesName[9:])
+		fmt.Println(fmt.Sprintf("Invalid %s number: %s\n", t, ns[typeLen:]))
 		return 0, false
 	}
-	return exercisesNo, true
+	return no, true
 }
